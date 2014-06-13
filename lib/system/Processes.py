8d67a47b8544bf29d9e286353a76d2a35531ea6d
Phaiax <phaiax@invisibletower.de>
2014-06-13 16:26:59 +0200

    refactoring and documenting

--- lib/system/Processes.py
+++ lib/system/Processes.py
@@ -112,7 +112,7 @@ class Processes(object):
 		return (i, dir)
 
 
-# ----------------------------------------- PROCESS (tss and adapter starter) ------------------------ #
+# ----------------------------------------- THREAD: (tss.js and adapter starter) ------------------------ #
 
 class TssJsStarterThread(Thread):
 	"""
@@ -153,99 +153,186 @@ class TssJsStarterThread(Thread):
 
 
 	def send_async_command(self, async_command):
-		""" send a AsyncCommand() instance to the adapter thread """
+		""" Send a AsyncCommand() instance to the adapter thread. """
 		self.tss_queue.put(async_command);
 
 	def kill_tssjs_queue_and_adapter(self):
 		"""
 			Tells adapter to leave syncronized queue and to finish 
-			and kills the tss.js process
+			and kills the tss.js process.
 		"""
 		self.tss_queue.put("stop!") # setinel value to stop queue
 		self.tss_process.kill()
 
 
 
-# ----------------------------------------- ASYNC PROCESS (adapter) -------------------------- #
+# ----------------------------------------- ADAPTER THREAD -------------------------- #
 
 class TssAdapterThread(Thread):
+	"""
+		This class recieves commands from syncroized queue, merges/debounces them,
+		executes them and then calls the async_command.callback in the the main_thread
+		afterwards with the help of sublime.set_timeout().
 
-	def __init__(self,stdin,stdout,queue):
+		This class uses a middleware queue (simple array) to allow modification of command order.
+		Every command form syncronized queue will immediatly be moved to middleware queue,
+		because pythons Queue.Queue() does not allow for anything else than pop and put.
+
+		Merging, debouncing and can then be done before poping the next command from middleware queue.
+
+		The thread block of syncronized queue will be used to wait for new commands.
+		To implement the debounce timeout, we add block release trigger commands
+		to the queue via sublime.set_timeout(). This releases the queue when the command
+		needs to be executed.
+
+		If the setinel string "stop!" arrives on the syncronized queue, this thread will finish.
+	"""
+
+	def __init__(self, stdin, stdout, queue):
+		"""
+			stdin, stdout: Connection to tss.js, 
+			queue: Synchronized queue to receive AsyncCommand instances.
+		"""
 		self.stdin = stdin
 		self.stdout = stdout
 		self.queue = queue
 		self.middleware_queue = []
 		Thread.__init__(self)
 
+	def run(self):
+		""" Working Loop. """
+		# block until queue is not empty anymore
+		# leave loop and finish thread if "stop!" arrives
+		for async_command in iter(self.queue.get, "stop!"): 
+			Debug('adapter', "CONTINUTE execution queue")
+
+			self.append_to_middlewarequeue(async_command)
+			self.add_pending_items_in_queue_to_middleware_queue()
+
+			# non blocking loop: work on middleware_queue and keep up-to-date with arriving commands
+			while not self.middleware_queue_is_finished():
+				self.pop_and_execute_from_middleware_queue()
+				self.add_pending_items_in_queue_to_middleware_queue()
+
+			# => enter thread block
+			Debug('adapter+', "WAIT for new work (%i currently debouncing)" % len(self.middleware_queue))
+
+		Debug('adapter', "QUIT async adapter to tss process and close queue")
+		self.stdin.close()
+		self.stdout.close()
+
+
+	def append_to_middlewarequeue(self, async_command, set_timer=True):
+		""" Append async_command and set timer to release queue if told and needed. """
+		self.middleware_queue.append(async_command)
+		Debug('adapter+', "APPEND to middleware (in %fs): %s" % (async_command.time_until_execution(), async_command.id))
+		if set_timer and not async_command.can_be_executed_now():
+			self.trigger_queue_block_release_for(async_command)
+
+
+	def trigger_queue_block_release_for(self, async_command):
+		""" Sets timeout to trigger a block release just when async_command can be executed. """
+		trigger_command = async_command.create_new_queue_trigger_command()
+		seconds = async_command.time_until_execution()
+		sublime.set_timeout(lambda: self.queue.put(trigger_command), int(seconds*1000) + 5)
+		Debug('adapter+', "TRIGGER QUEUE in %fs" % seconds)
+
+
 	def add_pending_items_in_queue_to_middleware_queue(self):
+		""" Uses the non blocking version of get() to pop from syncronized queue. """
 		try:
 			while(True):
 				self.append_to_middlewarequeue(self.queue.get_nowait())
 		except Empty:
 			pass
 
-	def tidy_middleware_queue_and_return_newest_item_with_same_id(self, async_command):
+
+	def middleware_queue_is_finished(self):
+		""" Returns True if no more commands or only debouncing commands pending on middleware queue. """
+		for cmd in self.middleware_queue:
+			if cmd.can_be_executed_now():
+				return False
+		return True
+
+
+	def pop_and_execute_from_middleware_queue(self):
+		""" Executes the next command, but merge it first. If merging with procrastinating enabled, do not execute it. """
+		if not self.middleware_queue_is_finished():
+			command_to_execute = self.middleware_queue.pop(0)
+			Debug('adapter', "POPPED from middleware: %s" % command_to_execute.id)
+
+			if command_to_execute.is_only_a_queue_trigger_command():
+				Debug('adapter+', "FOUND OLD TRIGGER object, don't execute anything")
+				return
+
+			command_to_execute = self.merge_cmd_on_middleware_queue_and_return_replacement(command_to_execute)
+			if command_to_execute: # can be None if merge_procrastinate() has defered current item
+				self.execute(command_to_execute)
+
+
+	def merge_cmd_on_middleware_queue_and_return_replacement(self, async_command):
+		""" Selecting merge behaviour and merge """
 		if(async_command.merge_behaviour == async_command.MERGE_IMMEDIATE):
 			return self.merge_immediate(async_command)
 		elif(async_command.merge_behaviour == async_command.MERGE_PROCRASTINATE):
 			return self.merge_procrastinate(async_command)
 
+
 	def merge_immediate(self, command):
-		# the newest elements are at the end of the queue array, 
-		# so start with the beginning
+		"""
+			Removes all elements with the same id from middleware_queue.
+			Remember: command is already poped from array, so there is no need to handle it
+			Returns the last added aka newest same-id command
+		"""
 		commands_to_remove = []
 		newest_command = command
 		for possible_replacement in self.middleware_queue: # from old to new
-			if possible_replacement.id == command.id: #command is already poped from array
-				newest_command.on_replaced(possible_replacement)
+			if possible_replacement.id == command.id: 
 				newest_command = possible_replacement
-				# remove all items with the same id
 				commands_to_remove.append(possible_replacement)
 
 		if len(commands_to_remove) > 0:
-			Debug('adapter+', "MERGED with %i (immediate): %s" % (len(commands_to_remove), command.id) )
-
-		for c in commands_to_remove:
-			self.middleware_queue.remove(c)
+			command.on_replaced(newest_command)
+			for c in commands_to_remove:
+				self.middleware_queue.remove(c)
+				if c.id != newest_command.id:
+					c.on_replaced(newest_command)
+			Debug('adapter+', "MERGED with %i other commands (immediate): %s" % (len(commands_to_remove), command.id) )
 
 		return newest_command
 
 
 	def merge_procrastinate(self, command):
-		# if there is another command with same id, then do not execute it now
-		# delete all same-id commands except for the last one(=newest) in the array
+		"""
+			If there is another command with same id, then do not execute it now.
+			Delete all same-id commands except for the last one(=newest) in the array.
+		"""
 		commands_to_remove = []
 		for possible_duplicate in self.middleware_queue: # from old to new
-			if possible_duplicate.id == command.id: #command is already poped from array
+			if possible_duplicate.id == command.id:
 				commands_to_remove.append(possible_duplicate) 
 
 		if len(commands_to_remove) > 0:						
-			commands_to_remove.pop() # don't delete newest duplicate command. 
+			newest_command = commands_to_remove.pop() # don't delete newest duplicate command. 
+			command.on_replaced(newest_command)
 			for c in commands_to_remove:
+				c.on_replaced(newest_command)
 				self.middleware_queue.remove(c)	
-			Debug('adapter+', "MERGED with %i (procr->defer): %s" % (len(commands_to_remove), command.id) )	
+			Debug('adapter+', "MERGED with %i other commands (procrastinated): %s" % (len(commands_to_remove), command.id) )	
 			return None # defer, no execution in this round
 		else:
 			return command # no defer, execute now, command has already been poped
 
-	def pop_and_execute_from_middleware_queue(self):
-		if not self.middleware_queue_is_finished():
-			command_to_execute = self.middleware_queue.pop(0)
-			Debug('adapter', "POPPED from middleware: %s" % command_to_execute.id)
-
-			if command_to_execute.id is "trigger":
-				Debug('adapter+', "FOUND OLD TRIGGER object, don't execute anything")
-				# this command has only used to trigger the queue block release
-				return
-
-			command_to_execute = self.tidy_middleware_queue_and_return_newest_item_with_same_id(command_to_execute)
-			if command_to_execute: # can be None if merge_procrastinate() has defered current item
-				self.execute(command_to_execute)
 
 	def execute(self, async_command):
+		"""
+			Executes Command.
+			If debouncing enabled und timeout not finished, add back to the end of the queue.
+			This may cause unexpected behaviour but should be unnoticed mostly.
+		"""
 		if not async_command.can_be_executed_now():
+			self.append_to_middlewarequeue(async_command, set_timer=True) # reappend to end
 			Debug('adapter+', "MOVED to end of queue, debouncing")
-			self.append_to_middlewarequeue(async_command, set_timer=False) # reappend to end
 			return
 
 		async_command.time_execute = time.time()
@@ -255,48 +342,6 @@ class TssAdapterThread(Thread):
 		# causes result callback to be called async
 		async_command.on_result(self.stdout.readline().decode('UTF-8'))
 
-	def middleware_queue_is_finished(self):
-		for cmd in self.middleware_queue:
-			if cmd.can_be_executed_now():
-				return False
-		return True
-
-	def trigger_queue_block_release_for(self, async_command):
-		trigger_command = async_command.create_new_queue_trigger_command()
-		seconds = async_command.time_until_execution()
-		sublime.set_timeout(lambda: self.queue.put(trigger_command), int(seconds*1000) + 5)
-		Debug('adapter+', "TRIGGER QUEUE in %fs" % seconds)
-
-	def append_to_middlewarequeue(self, async_command, set_timer=True):
-		self.middleware_queue.append(async_command)
-		Debug('adapter+', "APPEND to middleware (in %fs): %s" % (async_command.time_until_execution(), async_command.id))
-		if set_timer and async_command.time_until_execution() > 0:
-			self.trigger_queue_block_release_for(async_command)
-
-
-	def run(self):
-		# use a middleware queue for implementation of the
-		# behaviour described in AsyncCommand
-
-		# block until queue is not empty anymore
-		for async_command in iter(self.queue.get, "stop!"): 
-			Debug('adapter', "CONTINUTE execution queue")
-			self.append_to_middlewarequeue(async_command)
-			self.add_pending_items_in_queue_to_middleware_queue()	
-
-			while not self.middleware_queue_is_finished():
-				self.pop_and_execute_from_middleware_queue()
-				# stay up-to-date (but without entering the thread block,
-				# otherwise the middleware_queue can not be worked on)
-				self.add_pending_items_in_queue_to_middleware_queue()
-
-			# queue and middleware_queue are empty
-			# => enter thread block
-			Debug('adapter+', "WAIT for new work")
-
-		Debug('adapter', "QUIT async adapter to tss process and close queue")
-		self.stdin.close()
-		self.stdout.close()
 
 
 # -------------------------------------------- INIT ------------------------------------------ #
